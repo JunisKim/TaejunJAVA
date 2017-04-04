45936eef7bd8c4e198ddb4e2c8c67932211de5d0
initial commit <kim19911211@gmail.com>
2017-04-04 11:10:42 +0900

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by danawacomputer on 2017-04-04.
+ */
+public class Hello {
+    public static void main(String[] args){
+        int feeForBus = 1400;
+        System.out.println(feeForBus);
+    }
+
+}
---
+++ src/MyAccountBook.java
@@ -0,0 +1,15 @@
+/**
+ * Created by danawacomputer on 2017-04-04.
+ */
+public class MyAccountBook {
+    public static void main(String[] args) {
+
+    int busFee   = 1400;
+    int coffee   = 3500;
+    int forLunch = 7000;
+
+    int totalSpent = busFee + coffee + forLunch;
+
+    System.out.println("오늘 쓴 경비는 " + totalSpent + "원 입니다.");
+}
+}
